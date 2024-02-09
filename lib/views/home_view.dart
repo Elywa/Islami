044202b16309d5dbb270410c7105312f5eb4c2cc
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2024-02-09 22:21:32 +0200

    Added Bottom Navigation Bar

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,10 +1,84 @@
 import 'package:flutter/material.dart';
+import 'package:islami/views/hadeath_tab.dart';
+import 'package:islami/views/quran_tab.dart';
+import 'package:islami/views/radio_tab.dart';
+import 'package:islami/views/sebha_tab.dart';
 
-class HomeView extends StatelessWidget {
-  const HomeView({super.key});
+class HomeView extends StatefulWidget {
+  HomeView({super.key});
 
   @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> {
+  int selectedIndex = 0;
+  List<Widget> tabs = [
+    const QuranTab(),
+    const HadeathTab(),
+    const RadioTab(),
+    const SebhaTab()
+  ];
+  @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Container(
+      decoration: const BoxDecoration(
+          image: DecorationImage(
+        image: AssetImage('assets/scaffold_background.png'),
+        fit: BoxFit.fill,
+      )),
+      child: Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          centerTitle: true,
+          title: const Text(
+            'إسلامي',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 25,
+              fontFamily: 'ElMessiri',
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        bottomNavigationBar: Theme(
+          data: Theme.of(context).copyWith(
+            canvasColor: const Color(0xffB7935F),
+          ),
+          child: BottomNavigationBar(
+              selectedItemColor: Colors.black,
+              showUnselectedLabels: true,
+              currentIndex: selectedIndex,
+              onTap: (index) {
+                selectedIndex = index;
+                setState(() {});
+              },
+              items: const [
+                BottomNavigationBarItem(
+                    icon: ImageIcon(
+                      AssetImage('assets/radio.png'),
+                    ),
+                    label: 'Radio'),
+                BottomNavigationBarItem(
+                    icon: ImageIcon(
+                      AssetImage('assets/sebha_blue.png'),
+                    ),
+                    label: 'Sebha'),
+                BottomNavigationBarItem(
+                    icon: ImageIcon(
+                      AssetImage('assets/quran-quran-svgrepo-com.png'),
+                    ),
+                    label: 'Hadeath'),
+                BottomNavigationBarItem(
+                    icon: ImageIcon(
+                      AssetImage('assets/quran.png'),
+                    ),
+                    label: 'Quran'),
+              ]),
+        ),
+        body: tabs[selectedIndex],
+      ),
+    );
   }
 }
--- lib/views/splash_screen.dart
+++ lib/views/splash_screen.dart
@@ -10,7 +10,7 @@ class Splash extends StatelessWidget {
     // Delay for 3 seconds before navigating to the next screen
     Future.delayed(const Duration(seconds: 4), () {
       Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (_) => const HomeView()),
+        MaterialPageRoute(builder: (_) =>  HomeView()),
       );
     });
 
@@ -18,7 +18,7 @@ class Splash extends StatelessWidget {
       body: Container(
         decoration: const BoxDecoration(
           image: DecorationImage(
-            image: AssetImage('assets/splash_screen.png'),
+            image: AssetImage('assets/splash_light.png'),
             fit: BoxFit.fill,
           ),
         ),
