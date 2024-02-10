fc778dfbc984c58e010aab4ecd6c1945813ccddf
Ahmed Eliwa <63508508+Elywa@users.noreply.github.com>
2024-02-10 23:53:02 +0200

    Added Sebha UI

--- lib/models/sura_details_model.dart
+++
@@ -1,9 +0,0 @@
-class SuraDetailsModel {
-  final String name;
-  final int index;
-
-  const SuraDetailsModel({
-    required this.index,
-    required this.name,
-  });
-}
--- lib/views/sebha_tab.dart
+++ lib/views/sebha_tab.dart
@@ -1,30 +1,51 @@
 import 'package:flutter/material.dart';
 
 class SebhaTab extends StatelessWidget {
-  const SebhaTab({super.key});
+  const SebhaTab({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          child: Image.asset('assets/images/body of seb7a.png'),
-        ),
-        const Text(
-          'Number of Tasbesh',
-          style: TextStyle(
-            color: Colors.black,
-            fontSize: 25,
-            fontFamily: 'ElMessiri',
+    return Container(
+      color: Colors.transparent,
+      child: Column(
+        children: [
+          const Padding(
+            padding: EdgeInsets.only(bottom: 0),
+            child: Center(
+              child: Image(
+                width: 120,
+                height: 55,
+                image: AssetImage('assets/images/head of seb7a.png'),
+              ),
+            ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 20),
-          child: Container(
-            width: 60,
-            height: 80,
+          const Padding(
+            padding:
+                EdgeInsets.only(top: 0), // Adjust the top padding as needed
+            child: Image(
+              image: AssetImage('assets/images/body of seb7a.png'),
+            ),
+          ),
+          const SizedBox(
+            height: 25,
+          ),
+          const Padding(
+            padding: EdgeInsets.only(bottom: 20),
+            child: Text(
+              'عدد التسبيحات',
+              style: TextStyle(
+                color: Colors.black,
+                fontFamily: 'ElMessiri',
+                fontSize: 25,
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.only(bottom: 20),
+            width: 69,
+            height: 81,
             decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(50),
+              borderRadius: BorderRadius.circular(25),
               color: const Color(0xffB7935F),
             ),
             child: const Center(
@@ -32,28 +53,28 @@ class SebhaTab extends StatelessWidget {
                 '100',
                 style: TextStyle(
                   color: Colors.black,
-                  fontSize: 25,
-                  fontFamily: 'ElMessiri',
+                  fontSize: 20,
                 ),
               ),
             ),
           ),
-        ),
-        ElevatedButton(
-          style: ElevatedButton.styleFrom(
-              backgroundColor: const Color(0xffB7935F)),
-          onPressed: null,
-          child: const Text(
-            'سبحان الله',
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 20,
-              fontWeight: FontWeight.w400,
-              fontFamily: 'ElMessiri',
+          Padding(
+            padding: const EdgeInsets.only(top: 15),
+            child: TextButton(
+              style: TextButton.styleFrom(
+                  backgroundColor: const Color(0xffB7935F)),
+              onPressed: null,
+              child: const Text(
+                'سبحــان الله',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20,
+                ),
+              ),
             ),
-          ),
-        )
-      ],
+          )
+        ],
+      ),
     );
   }
 }
